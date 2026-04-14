0864ad460628f9aa31a1febf31ca13b53348c0da
jose <josetapia95@hotmail.com>
2026-04-13 20:01:38 -0500

    actualizando seguridad

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,16 @@ import { HomeComponent } from './components/home/home.component';
 import { ProfileDetailsComponent } from './components/profile-detail/profiledetail.component';
 import { LoginComponent} from './components/login/login.component';
 import { AdminComponent } from './components/admin/admin.component';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, 
   { path: 'perfil/:tipo', component: ProfileDetailsComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'admin', component: AdminComponent },
+  { 
+    path: 'admin', 
+    component: AdminComponent,
+    canActivate: [authGuard] 
+  },
   { path: '**', redirectTo: '' }
 ];
